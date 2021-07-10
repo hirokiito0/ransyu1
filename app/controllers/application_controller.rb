3810cd595b29ad5dd39ae61449759440075f5efa
hirokiito0 <syu.itou.h@gmail.com>
2021-07-10 10:54:02 +0000

    2[update]_top画面作成

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,16 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  # devise_controllerの機能（ユーザー登録、ログイン認証）行う場合
+  # 先に下記のconfigure_permitted_parametersの処理を行う
+  
+  
+  protected
+  
+  
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+    # ユーザー登録(sign_up)の際にユーザー名(name)のデータ処理を許可する
+    
+  end
+  
 end
